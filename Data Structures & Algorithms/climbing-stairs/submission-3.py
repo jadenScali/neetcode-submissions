3d4f4bbf5a1dd06174e34f255ef920ca692f8fcf
jaden <109479995+jadenScali@users.noreply.github.com>
2026-05-11 13:28:51 -0400

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.py
@@ -0,0 +1,8 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        
+        if n <= 2:
+            return n
+
+        return self.climbStairs(n-1) + self.climbStairs(n-2)
+        
