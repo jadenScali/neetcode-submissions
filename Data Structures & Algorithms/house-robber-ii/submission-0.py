d97a1665180547e119098953a8039def58f642c1
jaden <109479995+jadenScali@users.noreply.github.com>
2026-05-11 15:47:08 -0400

    Add: house-robber-ii - submission-0

---
+++ Data Structures & Algorithms/house-robber-ii/submission-0.py
@@ -0,0 +1,29 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        return max(self.rob_line(nums[:-1]), self.rob_line(nums[1:]))
+    
+    def rob_line(self, nums: list[int]):
+        memo = {}
+
+        def dfs(n: int):
+
+            result = 0
+            if n in memo:
+                return memo[n]
+                
+            if n == 0:
+                memo[n] = nums[n]
+            elif n == 1:
+                memo[n] = max(nums[0], nums[1])
+            else:
+                memo[n] = max(dfs(n-1), dfs(n-2) + nums[n])
+            
+            return memo[n]
+        
+        return dfs(len(nums)-1)
+
+
+
+
+
+        
