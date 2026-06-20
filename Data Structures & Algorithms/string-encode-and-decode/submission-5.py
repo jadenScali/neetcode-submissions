34fdb5e6505d6cdd623389141c4135d2a3f6f751
jaden <109479995+jadenScali@users.noreply.github.com>
2026-06-20 15:46:39 -0400

    Add: string-encode-and-decode - submission-5

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-5.py
@@ -0,0 +1,42 @@
+class Solution:
+
+    # abcd\r\ns1s2s3s4
+    def encode(self, strs: List[str]) -> str:
+        lengths = ""
+        literals = ""
+
+        if not strs:
+            return "0-\r\n"
+
+        for s in strs:
+            lengths += f'{str(len(s))}-'
+            literals += s
+        print(lengths)
+        
+        return f'{lengths}\r\n{literals}'
+
+
+    def decode(self, s: str) -> List[str]:
+        lengths = []
+        literals = ""
+        decoded = []
+
+        prev_r = False
+        for i, l in enumerate(s):
+            if l == '\n' and prev_r:
+                lengths = s[:i-2].split("-")
+                literals = s[i+1:]
+                break
+            elif l == '\r':
+                prev_r = True
+            else:
+                prev_r = False
+
+        i = 0
+        for l in lengths:
+            n = int(l)
+            decoded.append(literals[i : i + n])
+            i += n
+        
+        return decoded
+
