907d7cfe1edb0a85d038f4cf970849352480dbd6
jaden <109479995+jadenScali@users.noreply.github.com>
2026-06-26 22:22:13 -0400

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,27 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        s1_freq = {}
+        for l in s1:
+            s1_freq[l] = s1_freq.get(l, 0) + 1
+        
+        i = 0
+        n = len(s1)
+        m = len(s2)
+        while i + n - 1 < m:
+            winodw_freq = {}
+            for j in range(i, i + n):
+                l = s2[j]
+                winodw_freq[l] = winodw_freq.get(l, 0) + 1
+            
+            if winodw_freq == s1_freq:
+                return True
+            
+            i += 1
+        
+        return False
+
+
+
+
+
+        
