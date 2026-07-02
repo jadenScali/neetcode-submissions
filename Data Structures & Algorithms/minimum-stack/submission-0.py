3d8a743879cdb3f99f25e33582b472233cdbcb16
jaden <109479995+jadenScali@users.noreply.github.com>
2026-07-02 12:45:54 -0400

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        curr_min = self.getMin()
+        if curr_min:
+            curr_min = min(curr_min, val)
+        else:
+            curr_min = val
+            
+        self.stack.append((val, curr_min))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        if not self.stack:
+            return None
+
+        return self.stack[-1][1]
+        
